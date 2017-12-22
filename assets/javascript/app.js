bb1e67248c3a1ed8fe008d0aed8ccbf56599b757
Theodore hughes <theodorehughes@Theodores-MacBook-Pro.local>
2017-12-22 13:26:25 -0600

    app now creates train object and pushes it to firebase when submit button is clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,5 @@
+$(function(){
+
 // Initialize Firebase
   var config = {
     apiKey: "AIzaSyBdtFYMko3q7WUktpfrGVMIhE-631vAlFg",
@@ -8,3 +10,29 @@
     messagingSenderId: "910368069914"
   };
   firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+$("#add-train").on("click", function(){	
+
+	console.log("button clicked");
+	var name = $("#name-input").val().trim();
+	var destination = $("#destination-input").val().trim();
+	var firstTrainTime = $("#first-time-input").val().trim();
+	var frequency = $("#frequency-input").val().trim();
+
+	var train = {
+			name: name,
+			destination: destination,
+			firstTrainTime: firstTrainTime,
+			frequency: frequency
+		}
+
+	console.log("new train created: " + train);
+
+	database.ref().push(train);
+	console.log("pushed new train to firebase");
+
+});
+
+});
