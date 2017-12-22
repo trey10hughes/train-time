cf8670890a5684b8748a304c06cda952551cd5e6
Theodore hughes <theodorehughes@Theodores-MacBook-Pro.local>
2017-12-22 13:12:13 -0600

    basic html done, firebase initialized

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBdtFYMko3q7WUktpfrGVMIhE-631vAlFg",
+    authDomain: "train-time-3f3b0.firebaseapp.com",
+    databaseURL: "https://train-time-3f3b0.firebaseio.com",
+    projectId: "train-time-3f3b0",
+    storageBucket: "",
+    messagingSenderId: "910368069914"
+  };
+  firebase.initializeApp(config);
